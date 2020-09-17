3b082de83be06b9590d55297ca3070d9bb986e0d
Lars Vogel <Lars.Vogel@vogella.com>
2020-09-17 13:47:32 +0200

    Fix syntax error

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/BinaryOperation.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/BinaryOperation.java
@@ -7,7 +7,6 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 package org.eclipse.example.calc;
-ff
 /**
  * Binary operation interface
  */
